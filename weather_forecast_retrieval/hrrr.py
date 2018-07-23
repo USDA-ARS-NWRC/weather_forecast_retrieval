be088b07f0ec56f6439f20a250c4df0533a1de84
Micah Sandusky <micah.sandusky@ars.usda.gov>
2018-07-23 19:18:20 +0000

    downloading full grib files from ftp now, also updating to match changes in hrrr V3

--- weather_forecast_retrieval/hrrr.py
+++ weather_forecast_retrieval/hrrr.py
@@ -24,7 +24,7 @@ if PY3:  # pragma: no cover
 else:  # pragma: no cover
     from ConfigParser import SafeConfigParser
     from urllib import urlencode, urlretrieve
-   
+
 
 class HRRR():
     """
@@ -32,12 +32,12 @@ class HRRR():
     real-time 3 km resolution forecast. The model output is updated
     hourly and assimilates 3km radar. More information can be found
     at https://rapidrefresh.noaa.gov/hrrr/.
-    
+
     The class will download the 2D surface level products from the ftp
     site. The file format is a grib2 with a model cycle runtime and a
     forecast hour.
     """
-    
+
     ftp_url = 'ftp.ncep.noaa.gov'
     ftp_dir = '/pub/data/nccf/com/hrrr/prod'
     date_url = 'hrrr.%Y%m%d'
@@ -46,7 +46,7 @@ class HRRR():
 #     output_dir = '/data/snowpack/forecasts/hrrr'
 #     log_file = os.path.join(output_dir, 'hrrr.log')
     forecast_hours = [0, 1]
-    
+
     # the grib filter page with some of the default parameters
     grib_filter_url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_hrrr_2d.pl'
     grib_params = {
@@ -65,9 +65,9 @@ class HRRR():
         'toplat': 90,
         'bottomlat': 31
         }
-    
+
     num_threads = 20
-    
+
     var_map = {
         'air_temp': {
             'level': 2,
@@ -93,28 +93,28 @@ class HRRR():
             'parameterName': 'Low cloud cover'
             },
         }
-    
+
     var_elevation = {
         'typeOfLevel': 'surface',
         'parameterName': 'Geopotential height'
         }
-    
+
     def __init__(self, configFile=None, external_logger=None):
         """
         Args:
             configFile (str):  path to configuration file.
             external_logger: logger instance if using in part of larger program
         """
-        
+
         if configFile is not None:
             self.config = utils.read_config(configFile)
-            
+
             # parse the rest of the config file
             self.output_dir = self.config['output']['output_dir']
             self.grib_params.update(self.config['grib_parameters'])
             self.grib_subregion.update(self.config['grib_subregion'])
-        
-        
+
+
         # start logging
         if external_logger == None:
 
@@ -146,125 +146,128 @@ class HRRR():
 
             self._logger = logging.getLogger(__name__)
         else:
-            self._logger = external_logger       
-        
+            self._logger = external_logger
+
         self._logger.info('Initialized HRRR')
-    
+
     def retrieve_grib_filter(self):
         """
         Retrieve the data from the http with grib-filter. First read the ftp_url and
         determine what dates are available. Then use that to download
         the required data.
-        
+
         Use the ftp site to get a list of the directories to build the url's
         """
-        
+
         self._logger.info('Retrieving data from the http grib-filter')
-        
+
         ftp = FTP(self.ftp_url)
-        
+
         ftp.connect()
         self._logger.debug('Connected to FTP')
-        
+
         ftp.login()
         self._logger.debug('Logged into FTP')
-        
+
         ftp.cwd(self.ftp_dir)
         self._logger.debug('Changed directory to {}'.format(self.ftp_dir))
-        
+
         # get directory listing on server
         dir_list = ftp.nlst()
-        
+
         # go through the directory list and see if we need to add
         # any new data files
         for d in dir_list:
-            ftp_dir = os.path.join(self.ftp_dir, d)
+            ftp_dir = os.path.join(self.ftp_dir, d, 'conus')
             self._logger.info('Changing directory to {}'.format(ftp_dir))
-            
+
             # get the files in the new directory
             ftp.cwd(ftp_dir)
             ftp_files = ftp.nlst()
-            
+
             # check if d exists in output_dir
             out_path = os.path.join(self.output_dir, d)
             if not os.path.isdir(out_path):
                 os.mkdir(out_path)
                 self._logger.info('mkdir {}'.format(out_path))
-            
+
             # get all the files that match the file filter
             wanted_files = fnmatch.filter(ftp_files, self.file_filter)
-            
+
             # go through each file, see if it exists, and retrieve if not
             threads = []
             for f in wanted_files:
                 file_local = os.path.join(out_path, f)
 #                 remote_file = os.path.join(ftp_dir, f)
-                
+
                 self.grib_params['dir'] = '/{}'.format(d)
                 self.grib_params['file'] = f
                 p = urlencode(self.grib_params)
-                
-                remote_url = '{}?{}&subregion=&{}'.format(self.grib_filter_url, p, urlencode(self.grib_subregion))
-                
+
+                remote_url = '{}?{}&subregion=&{}'.format(self.grib_filter_url,
+                                                          os.path.join(p,'conus'),
+                                                          urlencode(self.grib_subregion))
+
                 # get the file if it doesn't exist
                 if not os.path.exists(file_local):
                     self._logger.info('Adding {}'.format(f))
                     t = threading.Thread(target=urlretrieve, args=(remote_url, file_local))
                     t.start()
                     threads.append(t)
-                    
+
                 if len(threads) == self.num_threads:
                     self._logger.info('Getting {} files'.format(self.num_threads))
                     for t in threads:
                         t.join()
-                    threads = []          
-            
+                    threads = []
+
         ftp.close()
         self._logger.info('Done retrieving files')
-        
-        
+
+
     def retrieve_ftp(self):
         """
         Retrieve the data from the ftp site. First read the ftp_url and
         determine what dates are available. Then use that to download
         the required data.
         """
-        
+
         self._logger.info('Retrieving data from the ftp site')
         ftp = FTP(self.ftp_url)
-        
+
         ftp.connect()
         self._logger.debug('Connected to FTP')
-        
+
         ftp.login()
         self._logger.debug('Logged into FTP')
-        
+
         ftp.cwd(self.ftp_dir)
         self._logger.debug('Changed directory to {}'.format(self.ftp_dir))
-        
+
         # get directory listing on server
         dir_list = ftp.nlst()
-        
+
         # go through the directory list and see if we need to add
         # any new data files
         for d in dir_list:
-            ftp_dir = os.path.join(self.ftp_dir, d)
+            ftp_dir = os.path.join(self.ftp_dir, d, 'conus')
             self._logger.info('Changing directory to {}'.format(ftp_dir))
-            
+
             # get the files in the new directory
             ftp.cwd(ftp_dir)
             ftp_files = ftp.nlst()
-            
+
             # check if d exists in output_dir
             out_path = os.path.join(self.output_dir, d)
             if not os.path.isdir(out_path):
                 os.mkdir(out_path)
                 self._logger.info('mkdir {}'.format(out_path))
-            
-            for fhr in self.forecast_hours:
-            
+
+            forecast_hours = range(24)
+            for fhr in forecast_hours:
+
                 wanted_files = fnmatch.filter(ftp_files, self.file_name.format(fhr))
-                
+
                 self._logger.debug('Found {} files matching pattern'.format(len(wanted_files)))
 
                 # go through each file and see if it exists, retrieve if not
@@ -276,37 +279,37 @@ class HRRR():
                         h = open(out_file, 'wb')
                         ftp.retrbinary('RETR {}'.format(ftp_file), h.write)
                         h.close()
-                    
+
         ftp.close()
-                
-              
+
+
     def get_saved_data(self, start_date, end_date, bbox, output_dir=None, var_map=None, forecast=[0], force_zone_number=None):
         """
         Get the saved data from above for a particular time and a particular
         bounding box.
-        
+
         Args:
             start_date: datetime for the start
             end_date: datetime for the end
             bbox: list of  [lonmin,latmin,lonmax,latmax]
             var_map: dictionary to map the desired variables into {new_variable: hrrr_variable}
             forecast: true or false, whether or not to grab the forecat for the start_date
-        
+
         Returns:
             List containing dataframe for the metadata for each node point for the desired variables
         """
-        
+
         if start_date > end_date:
             raise ValueError('start_date before end_date')
-        
+
         self._logger.info('getting saved data')
         if var_map is None:
             self._logger.warn('var_map not specified, will return default outputs!')
-        
+
         self.force_zone_number = force_zone_number
         if output_dir is not None:
             self.output_dir = output_dir
-        
+
         # find all the data in between the two dates
         d = start_date.date()
         delta = timedelta(days=1)
@@ -315,67 +318,67 @@ class HRRR():
             # get the path for the upper level directory
             p = d.strftime(self.date_url)
             self._logger.debug('Found directory {}'.format(p))
-            
+
             # find all the files in the directory
             for f in forecast:
                 fname = self.file_name.format(f)
-                
+
                 pth = os.path.join(self.output_dir, p, fname)
                 fmatch += glob.glob(pth)
-            
+
             d += delta
-        
+
         # load in the data for the given files and bounding box
         idx = None
         df = {}
         for f in fmatch:
             self._logger.debug('Reading {}'.format(f))
             gr = pygrib.open(f)
-            
+
             # if this is the first run, then find out a few things about the grid
             if idx is None:
                 metadata, idx = self.get_metadata(gr, bbox)
-                
+
                 # create all of the dataframes for each mapped variable
                 for k in self.var_map.keys():
                     df[k] = pd.DataFrame(columns=metadata.index)
-            
+
             for key,params in self.var_map.items():
                 g = gr.select(**params)
-                
+
                 if len(g) > 1:
                     raise Exception('variable map returned more than one message for {}'.format(key))
                 g = g[0]
-             
+
                 # get the data, ensuring that the right location is grabbed
                 dt = g.validDate
                 if dt >= start_date and dt <= end_date:
                     df[key].loc[dt,:] = g.values[idx]
-          
-        
+
+
         for key in df.keys():
             df[key].sort_index(axis=0, inplace=True)
             if key == 'air_temp':
                 df['air_temp'] -= 273.15
             if key == 'cloud_factor':
                 df['cloud_factor'] = 1 - df['cloud_factor']/100
-                    
+
         return metadata, df
-             
+
     def get_metadata(self, gr, bbox):
         """
         Generate a metadata dataframe from the elevation data
-        
+
         Args:
             gr: pygrib.open() object
             bbox: list of the bounding box
-            
+
         Returns:
             dataframe for the metadata
         """
-        
+
         elev, lat, lon = gr.select(**self.var_elevation)[0].data()
-                
+
         # subset the data
         idx = (lon >= bbox[0]) & (lon <= bbox[2]) & (lat >= bbox[1]) & (lat <= bbox[3])
         lat = lat[idx]
@@ -384,10 +387,10 @@ class HRRR():
         a = np.argwhere(idx)
         primary_id = ['grid_y%i_x%i' % (i[0], i[1]) for i in a]
         self._logger.info('Found {} grid cells within bbox'.format(len(a)))
-        
+
         if len(a) == 0:
             raise ValueError('Did not find any grid cells within bbox')
-        
+
         metadata = pd.DataFrame(index=primary_id,
                                 columns=('utm_x', 'utm_y', 'latitude',
                                          'longitude', 'elevation'))
@@ -396,10 +399,10 @@ class HRRR():
         metadata['elevation'] = elev.flatten()
         metadata = metadata.apply(apply_utm,
                                   args=(self.force_zone_number,),
-                                  axis=1)  
-        
+                                  axis=1)
+
         return metadata, idx
-                
+
 def apply_utm(s, force_zone_number):
     """
     Calculate the utm from lat/lon for a series
@@ -413,9 +416,9 @@ def apply_utm(s, force_zone_number):
                         force_zone_number=force_zone_number)
     s['utm_x'] = p[0]
     s['utm_y'] = p[1]
-    return s          
-        
-        
+    return s
+
+
 if __name__ == '__main__':
     pass
 #     HRRR().retrieve_grib_filter()
@@ -424,8 +427,3 @@ if __name__ == '__main__':
 #     end_date = datetime(2017, 12, 2, 5, 0, 0)
 #     bbox =  [-120.13, 37.63, -119.06, 38.3]
 #     HRRR().get_saved_data(start_date, end_date, bbox, force_zone_number=11)
-    
-    
-    
-    
-    
