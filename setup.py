f3daa992448397d735dba8f64d68161e2234a948
Scott Havens <scotthavens42@gmail.com>
2019-08-09 12:47:52 -0600

    updating the requirements, setup and makefile for pypi uploading

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@
 # # from setuptools import setup, find_packages
 from setuptools import setup
 
-# with open('README.md') as readme_file:
-#     readme = readme_file.read()
+with open('README.md') as readme_file:
+    readme = readme_file.read()
 
 # with open('HISTORY.rst') as history_file:
 #     history = history_file.read()
 
-# requirements = [
-#     # TODO: put package requirements here
-# ]
+with open('requirements.txt') as req_file:
+    requirements = req_file.read()
 
 # setup_requirements = [
 #     # TODO(scotthavens): put setup requirements (distutils extensions, etc.) here
@@ -28,7 +27,8 @@ setup(
     name='weather_forecast_retrieval',
     version='0.6.0',
     description="Weather forecast retrieval gathers relavant gridded weather forecasts to ingest into physically based models for water supply forecasts",
-    # long_description=readme + '\n\n' + history,
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="Scott Havens",
     author_email='scott.havens@ars.usda.gov',
     url='https://github.com/USDA-ARS-NWRC/weather_forecast_retrieval',
@@ -38,14 +38,16 @@ setup(
             'grib2nc=weather_forecast_retrieval.grib2nc:main'
         ]},
     include_package_data=True,
+    install_requires=requirements,
     license="CC0 1.0",
     zip_safe=False,
     keywords='weather_forecast_retrieval',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     scripts=[
