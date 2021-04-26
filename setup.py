89f3de7572c0bebdd43b094352d9edbc53fec0ef
Scott Havens <scott.havens@ars.usda.gov>
2021-04-26 15:12:41 -0600

    another update

--- setup.py
+++ setup.py
@@ -32,8 +32,11 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     test_suite='tests',
     extras_require={
